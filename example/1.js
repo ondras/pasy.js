eab9705d123aff090275b81554db2705403944b9
Ondrej Zara <ondrej.zara@gmail.com>
2015-02-06 14:04:30 +0100

    color experiments

--- example/1.js
+++ example/1.js
@@ -5,12 +5,12 @@ var ps = scene.particleSet();
 ps
 	.pointSize(3)
 	.count(100000)
-	.color([1, 0.5, 0])
+	.color([.8, .4, .1])
 	.attribute("velocity", 3, function(gl, count) {
 		return pasy.randomSet(count, vec3, 0.05);
 	})
-	.color(function(gl, count) {
+/*	.color(function(gl, count) {
 		return pasy.randomSet(count, vec3, 0);
 	});
-
+*/
 scene.add(ps);
